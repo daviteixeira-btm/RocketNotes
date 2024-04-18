e7af4702634e6e387a156c5c483e4013d97178d7
Davi Teixeira <daviteixeira.btm@gmail.com>
2024-04-18 00:21:26 -0300

    [Davi] Tag Component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,6 +2,7 @@ import { Container, Links } from "./styles"
 import { Header } from "../../Components/Header"
 import { Button } from "../../Components/Button"
 import { Section } from "../../Components/Section"
+import { Tag } from "../../Components/Tag"
 
 export function Details(){
   return (
@@ -16,8 +17,10 @@ export function Details(){
       </Section>
 
       <Section title="Marcadores">
-        
+        <Tag title="express" />
+        <Tag title="nodejs" />
       </Section>
+      
       <Button title="Voltar"/>
     </Container>
   )
