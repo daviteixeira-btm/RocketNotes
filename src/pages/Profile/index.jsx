2f159f3374d372d902f7b38b09a74b2ce6809fb6
Davi Teixeira <daviteixeira.btm@gmail.com>
2024-04-20 00:33:39 -0300

    [Davi] User Profile Page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,5 @@
-import { Container, Form } from "./styles";
-import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi";
+import { Container, Form, Avatar } from "./styles";
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
 import { Input } from "../../Components/Input";
 import { Button } from "../../Components/Button";
 
@@ -13,6 +13,23 @@ export function Profile(){
             </header>
 
             <Form>
+
+                <Avatar>
+                    <img 
+                        alt="Foto do usuário" 
+                        src="https://github.com/daviteixeira-btm.png" 
+                    />
+
+                    <label htmlFor="avatar">
+                        <FiCamera />
+                        
+                        <input 
+                            id="avatar"
+                            type="file"
+                        />
+                    </label>
+                </Avatar>
+
                 <Input
                     type="text"
                     icon={FiUser} 
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -20,5 +20,46 @@ export const Container = styled.div`
 
 export const Form = styled.form`
     max-width: 340px;
-    margin: 30px auto 0;
+    margin: -95px auto 0;
+
+    > div:nth-child(4){
+        margin-top: 24px; 
+    }
+`;
+
+export const Avatar = styled.div`
+    width: 166px;
+    height: 166px;
+    position: relative;
+    margin: 0 auto 32px;
+
+    > img {
+        width: 166px;
+        height: 166px;
+        border-radius: 50%;
+    }
+
+    > label {
+        right: 7px;
+        bottom: 7px;
+        width: 48px;
+        height: 48px;
+        display: flex;
+        cursor: pointer;
+        position: absolute;
+        border-radius: 50%;
+        align-items: center;
+        justify-content: center;
+        background-color: ${({ theme }) => theme.COLORS.ORANGE};
+
+        input {
+            display: none;
+        }
+
+        svg {
+            width: 20px;
+            height: 20px;
+            color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+        }
+    }
 `;
