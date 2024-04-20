f781316a5630fb6f496050b1c99836efa86f7e9f
Davi Teixeira <daviteixeira.btm@gmail.com>
2024-04-20 01:30:35 -0300

    [Davi] New Note Page

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,25 @@
+import { Header } from "../../Components/Header";
+import { Input } from "../../Components/Input";
+
+import { Container, Form } from "./styles";
+
+export function New(){
+    return (
+        <Container>
+            <Header />
+
+            <main>
+                <Form>
+                    <header>
+                        <h1>Criar nota</h1>
+                        <a href="/">voltar</a>
+                    </header>
+
+                    <Input
+                        placeholder="Título"
+                    />
+                </Form>
+            </main>
+        </Container>
+    )
+}
