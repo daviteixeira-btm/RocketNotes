c7210849333d4467ee2f2063530d0002710897e7
Davi Teixeira <daviteixeira.btm@gmail.com>
2024-04-27 22:57:45 -0300

    [Davi] Integrating the Front-end with the Back-end - (Route Redirection)

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
 
+// Importamos o Hook de autenticação
+import { useAuth } from "../hooks/auth";
+
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes(){
+    // para acessar nosso usuário
+    const { user } = useAuth();
+
     return (
         <BrowserRouter>
-            <AuthRoutes />
+            {/*Aqui fazemos o redirecionamento de rotas se o usuário está conectado ou não */}
+            {user ? <AppRoutes /> : <AuthRoutes />}
         </BrowserRouter>
     )
 }
