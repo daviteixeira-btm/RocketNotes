1d8a5788f1ce7b96674db422d2750a08451b6355
Davi Teixeira <daviteixeira.btm@gmail.com>
2024-05-02 01:04:22 -0300

    [Davi] Integrating the Front-end with the Back-end - (Adding link in note)

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Header } from "../../Components/Header";
 import { Input } from "../../Components/Input";
 import { Textarea } from "../../Components/Textarea";
@@ -11,6 +12,21 @@ import { Button } from "../../Components/Button";
 import { Link } from "react-router-dom";
 
 export function New(){
+
+    // Aqui será o estado que guarda todos os links
+    const [links, setLinks] = useState([]);
+
+    // Onde guardamos o novo link adicionado no momento
+    const [newLink, setNewLink] = useState("");
+
+    // Função que atualiza o nosso vetor de links, acessando tudo que tinha antes com o link novo
+    function handleAddLink(){
+        //acessamos o estado anterior para montar um novo veto, adicionando o novo link
+        setLinks(prevState => [...prevState, newLink]);
+        //Depois limpamos o estado
+        setNewLink("");
+    }
+
     return (
         <Container>
             <Header />
@@ -29,8 +45,22 @@ export function New(){
                     <Textarea placeholder="Observações" />
 
                     <Section title="Links úteis">
-                        <NoteItem value="https://rocketseat.com.br" />
-                        <NoteItem isNew placeholder="Novo link" />
+                        {
+                            links.map((link, index) => (
+                                <NoteItem 
+                                    key={String(index)} 
+                                    value={link}
+                                    onClick={() => { }}
+                                />
+                            ))
+                        }
+                        <NoteItem 
+                            isNew
+                            value={newLink}
+                            placeholder="Novo Link"
+                            onClick={handleAddLink}
+                            onChange={event => setNewLink(event.target.value)}
+                        />
                     </Section>
 
                     <Section title="Marcadores">
