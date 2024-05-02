7efde48d473b196edd85bba075a3573d2289d02e
Davi Teixeira <daviteixeira.btm@gmail.com>
2024-05-02 20:58:07 -0300

    [Davi] Integrating the Front-end with the Back-end - (loading search tags)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { FiPlus, FiSearch } from "react-icons/fi";
 
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
@@ -7,8 +8,24 @@ import { Input } from "../../Components/Input";
 import { Note } from "../../Components/Note";
 import { Section } from "../../Components/Section"
 import { ButtonText } from "../../Components/ButtonText";
+import { api } from "../../services/api";
 
 export function Home(){
+
+    const [tags, setTags] = useState([]);
+
+    useEffect(() => {
+        // Funcionalidade de carregar as tags que servem de filtro de pesquisa
+        async function fetchTags(){
+            // Pegamos o resultado da api
+            const response = await api.get("/tags");
+            // Armaznamos os dados da resposta
+            setTags(response.data);
+        }
+
+        fetchTags();
+    }, []);
+
     return (
         <Container>
             <Brand>
@@ -18,15 +35,12 @@ export function Home(){
             <Header />
 
             <Menu>
-                <li>
-                    <ButtonText title="Todos" isactive />
-                </li>
-                <li>
-                    <ButtonText title="React" />
-                </li>
-                <li>
-                    <ButtonText title="Node" />
-                </li>
+                <li><ButtonText title="Todos" isactive /></li>
+                {
+                    tags && tags.map(tag => (
+                        <li key={String(tag.id)}><ButtonText title={tag.name} /></li>
+                    ))
+                }
             </Menu>
 
             <Search>
