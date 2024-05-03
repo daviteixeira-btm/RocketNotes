2ee46dcd6aa3c3100db886e5f565b3e9adf847e7
Davi Teixeira <daviteixeira.btm@gmail.com>
2024-05-03 03:17:22 -0300

    [Davi] Integrating the Front-end with the Back-end - (Handling Unavailable Routes)

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,13 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import { SingIn } from "../pages/SingIn";
 import { SingUp } from "../pages/SingUp";
 
 export function AuthRoutes(){
+
+    const user = localStorage.getItem("@rocketnotes:user");
+
     return (
         <Routes>
             <Route path="/" element={<SingIn />} />
             <Route path="/register" element={<SingUp />} />
+
+            {/* Rota de redirecionamento para a de SingIn caso o usuário digite uma url inválida */}
+            {/* Caso alguma nota seja salva nos favoritos, nos apenas redirecionamos se o usuário for nulo */}
+            { !user && <Route path="*" element={<Navigate to="/" />} /> }
         </Routes>
     )
 }
